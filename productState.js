6b9f5e17ad456eace8b904ed065a4d5ecf6508a5
stephfrancois <francoistephane@gmail.com>
2026-06-10 17:29:59 -0400

    removed  file

--- productState.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var model = "Nike Dunk Low 'Game Royal Navy'";
-var brand = "Nike";
-var price = 86;
-var discount = 10;
-var finalPrice = price - discount;
-var cartQuantity = 0;
-cartQuantity++;
