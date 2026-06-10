a097449d67f579f889b3e4fe88b33669bc69fe3e
stephfrancois <francoistephane@gmail.com>
2026-06-10 10:37:07 -0400

    feat: product state 1

---
+++ productState.js
@@ -0,0 +1,8 @@
+"use strict";
+var model = "Nike Dunk Low 'Game Royal Navy'";
+var brand = "Nike";
+var price = 86;
+var discount = 10;
+var finalPrice = price - discount;
+var cartQuantity = 0;
+cartQuantity++;
---
+++ productState.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+const model= "Nike Dunk Low 'Game Royal Navy'";
+const brand = "Nike";
+const price = 86;
+const discount = 10;
+const finalPrice: number= price - discount;
+
+let cartQuantity:number = 0;
+cartQuantity++; 
